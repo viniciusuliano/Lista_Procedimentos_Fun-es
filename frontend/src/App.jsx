f8f989d697a9b4c3cc7876d08474bd37fdae77ff
JbSense <dev.jvxavier@gmail.com>
2022-05-26 20:48:25 -0300

    Criado o header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Header from './components/body/Header';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header/>
+
         <Routes>
           <Route path="/" element={<Home/>}/>
         </Routes>
